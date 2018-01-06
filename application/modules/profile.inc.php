fe25d38ed9189a1e5514234585900e7c4af973fc
killian wolfger <killianwolfger@hotmail.fr>
2018-01-06 20:42:20 +0100

    test

--- application/modules/profile.inc.php
+++ application/modules/profile.inc.php
@@ -1,3 +1,15 @@
+<?php
+
 $user['nom'] = "wolfger";
 $user['prenom'] = "killian";
 $data['user'] = $user;
+
+foreach ($data as $value)
+{
+	foreach ($value as $value2)
+	{
+		echo "Valeur : $value2<br/>";
+	}
+}
+
+?>
--- application/views/modules/accueil.tpl
+++ application/views/modules/accueil.tpl
@@ -1,3 +1,7 @@
+<?php
+
 {extends file = "../layout.tpl"}
 <a>cette ligne se trouve dans acceuil.tpl voila voila</a>
 {include file = "../../application/modules/acceuil.inc.php"}
+
+?>
