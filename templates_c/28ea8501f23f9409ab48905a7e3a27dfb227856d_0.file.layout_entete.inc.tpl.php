03ed5c7b8ae527d2afe283e383abe42b600f7f4e
killian wolfger <killianwolfger@hotmail.fr>
2018-01-07 00:47:26 +0100

    database

---
+++ templates_c/28ea8501f23f9409ab48905a7e3a27dfb227856d_0.file.layout_entete.inc.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 3.1.30, created on 2018-01-07 00:22:04
+  from "C:\xampp\htdocs\KibaJdr\application\views\layout\layout_entete.inc.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5a515a1ce94aa6_83394896',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '28ea8501f23f9409ab48905a7e3a27dfb227856d' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\KibaJdr\\application\\views\\layout\\layout_entete.inc.tpl',
+      1 => 1515261115,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5a515a1ce94aa6_83394896 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<h1>Kiba jeu de rôle</h1>
+<p>pour s'amuser, il faut jouer</p><?php }
+}
---
+++ templates_c/cc4514971e0c2efa7d9a8abc6cac66d10f2f93b8_0.file.layout_pied.inc.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.30, created on 2018-01-07 00:22:04
+  from "C:\xampp\htdocs\KibaJdr\application\views\layout\layout_pied.inc.tpl" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5a515a1cea16f7_22532004',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'cc4514971e0c2efa7d9a8abc6cac66d10f2f93b8' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\KibaJdr\\application\\views\\layout\\layout_pied.inc.tpl',
+      1 => 1515261115,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5a515a1cea16f7_22532004 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<h2>foot-layout c'est ici</h2><?php }
+}
