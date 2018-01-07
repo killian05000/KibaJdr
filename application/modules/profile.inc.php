73e5010e2d4323de80e5d702be91f107458f2b38
unknown <baptiste.barbe@gmail.com>
2018-01-07 14:42:47 +0100

    tst

---
+++ application/modules/profile.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$user['nom'] = "wolfger";
+$user['prenom'] = "killian";
+$data['user'] = $user;
+
+/*
+foreach ($data as $value)
+{
+	foreach ($value as $value2)
+	{
+		echo "Valeur : $value2<br/>";
+	}
+}
+*/
+?>
